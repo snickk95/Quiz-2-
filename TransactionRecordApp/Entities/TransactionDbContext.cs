8c1b8aeb0b4a5713bae20f1db2cfc2a1cd5fcdcb
snickk95 <51108751+snickk95@users.noreply.github.com>
2024-02-01 19:29:31 -0500

    updated db context

--- TransactionRecordApp/Entities/TransactionDbContext.cs
+++ TransactionRecordApp/Entities/TransactionDbContext.cs
@@ -22,5 +22,46 @@ namespace TransactionRecordApp.Entities
             : base(options)
         {
         }
+        //way to access the transactions
+        public DbSet<Transaction> Transactions { get; set; }
+        
+        //used to seed data base with 4 entries
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>().HasData(
+               new Transaction()
+               {
+                   TransactionId = 1,
+                   TickerSymbol = "AAPL",
+                   CompanyName = "Apple",
+                   Quantity = 2,
+                   sharePrice = 194.62
+               },
+               new Transaction()
+               {
+                   TransactionId = 2,
+                   TickerSymbol = "F",
+                   CompanyName = "Ford Motors Company",
+                   Quantity = 4,
+                   sharePrice = 11.34
+               },
+               new Transaction()
+               {
+                   TransactionId = 3,
+                   TickerSymbol = "GOOG",
+                   CompanyName = "Alphabet Inc.",
+                   Quantity = 100,
+                   sharePrice = 146.51
+               },
+               new Transaction()
+               {
+                   TransactionId = 4,
+                   TickerSymbol = "MSFT",
+                   CompanyName = "Microsoft Corporation",
+                   Quantity = 100,
+                   sharePrice = 397.21
+               }
+           );
+        }
     }
 }
